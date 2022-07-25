323701feb88b5c66a88191b128d563325993ee67
TashariWebDev <ridwan@tashari.co.za>
2022-07-25 16:17:20 +0200

    deploy

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://www.admin.vapecrew.co.za'),
     'admin_url' => env('ADMIN_URL', 'http://localhost'),
     'asset_url' => env('ASSET_URL'),
 
