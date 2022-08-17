d05e5b33c9d594f717090910d9122223ab73d618
TashariWebDev <ridwan@tashari.co.za>
2022-08-17 12:09:21 +0200

    fixed layout issue on inventory page product-listing

--- resources/views/components/product-listing-simple.blade.php
+++ resources/views/components/product-listing-simple.blade.php
@@ -4,15 +4,15 @@
 
 
 <div>
-    <p class="font-medium text-gray-500 text-xs">
+    <p class="font-medium text-gray-600 text-xs">
         {{ $product->sku }}
     </p>
-    <p class="font-semibold text-gray-800 text-sm">
+    <p class="font-bold text-gray-800 text-sm">
         {{ $product->brand }} {{ $product->name }}
     </p>
     <div class="flex space-x-1 items-center">
         @foreach($product->features as $feature)
-            <p class="text-xs text-gray-600 pr-1 @if(!$loop->last) border-r @endif"
+            <p class="font-medium text-xs text-gray-600 pr-1 @if(!$loop->last) border-r @endif"
             > {{ $feature->name }}</p>
         @endforeach
     </div>
