d3c73edb9e4b6d2fc1a02a3cb4233f7b61f560be
TashariWebDev <ridwan@tashari.co.za>
2022-10-01 07:47:01 +0200

    Add supplier credit module

--- resources/views/livewire/reports/index.blade.php
+++ resources/views/livewire/reports/index.blade.php
@@ -29,27 +29,27 @@
                 </dd>
             </div>
 
-            @if($purchases->total_purchases)
-                <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-                    <dt class="text-sm font-medium text-gray-500 truncate">Purchases</dt>
-                    <dd class="mt-1 tracking-tight font-semibold text-gray-900">
-                        {{ number_format(to_rands(ex_vat($purchases->total_purchases)),2) ?? '0.00' }}
-                    </dd>
-                    <dd class="mt-1 text-sm tracking-tight font-semibold text-gray-500">
-                        {{ number_format(to_rands($purchases->total_purchases),2) ?? '0.00' }}
-                    </dd>
-                </div>
-            @else
-                <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
-                    <dt class="text-sm font-medium text-gray-500 truncate">Purchases</dt>
-                    <dd class="mt-1 tracking-tight font-semibold text-gray-900">
-                        '0.00'
-                    </dd>
-                    <dd class="mt-1 text-sm tracking-tight font-semibold text-gray-500">
-                        '0.00'
-                    </dd>
-                </div>
-            @endif
+            {{--            @if($purchases->total_purchases)--}}
+            <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+                <dt class="text-sm font-medium text-gray-500 truncate">Purchases</dt>
+                <dd class="mt-1 tracking-tight font-semibold text-gray-900">
+                    {{ number_format(to_rands(ex_vat($purchases->total_purchases) ?? '0.00'),2) ?? '0.00' }}
+                </dd>
+                <dd class="mt-1 text-sm tracking-tight font-semibold text-gray-500">
+                    {{ number_format(to_rands($purchases->total_purchases ?? '0.00'),2) ?? '0.00' }}
+                </dd>
+            </div>
+            {{--            @else--}}
+            {{--                <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">--}}
+            {{--                    <dt class="text-sm font-medium text-gray-500 truncate">Purchases</dt>--}}
+            {{--                    <dd class="mt-1 tracking-tight font-semibold text-gray-900">--}}
+            {{--                        '0.00'--}}
+            {{--                    </dd>--}}
+            {{--                    <dd class="mt-1 text-sm tracking-tight font-semibold text-gray-500">--}}
+            {{--                        '0.00'--}}
+            {{--                    </dd>--}}
+            {{--                </div>--}}
+            {{--            @endif--}}
 
             <div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
                 <dt class="text-sm font-medium text-gray-500 truncate">Expenses</dt>
