d106cfbe4ceaacd4431dde4fcdb066422a6e9024
Ridwan Guman <info@dezinehq.com>
2023-08-16 21:03:51 +0200

    Remove delay from transaction queue

--- app/Console/Commands/UpdateTransactionsCommand.php
+++ app/Console/Commands/UpdateTransactionsCommand.php
@@ -17,13 +17,15 @@ class UpdateTransactionsCommand extends Command
         $customer = Customer::find($this->argument('customer'));
 
         if ($customer) {
-            UpdateCustomerRunningBalanceJob::dispatch($customer->id)->delay(now()->addMinute(1));
+            UpdateCustomerRunningBalanceJob::dispatch($customer->id);
         } else {
             $customers = Customer::all();
 
             foreach ($customers as $customer) {
-                UpdateCustomerRunningBalanceJob::dispatch($customer->id)->delay(now()->addMinute(1));
+                UpdateCustomerRunningBalanceJob::dispatch($customer->id);
             }
         }
+
+        $this->info('complete');
     }
 }
