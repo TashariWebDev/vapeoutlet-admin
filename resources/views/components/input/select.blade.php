2a7ddb69cb117c3eb8a1834482132ddfc6ae863e
Ridwan Guman <info@dezinehq.com>
2023-12-17 21:00:34 +0200

    Add abandoned orders to Orders screen

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -1,8 +1,13 @@
-<!--suppress HtmlFormInputWithoutLabel -->
-<div>
-    <select
-        {{ $attributes->merge(['class' => 'block w-full rounded-md px-4 py-1.5 bg-slate-100 dark:text-slate-100 dark:bg-slate-800 text-slate-700 border-slate-200 dark:border-slate-800 focus:border-ring focus:ring-sky-400 text-sm']) }}
-    >
-        {{ $slot }}
-    </select>
-</div>
+@props([
+    'label' => ''
+])
+<label>
+  <x-input.label>
+    {{ $label }}
+  </x-input.label>
+  <select
+      {{ $attributes->merge(['class' => 'block w-full rounded-md px-4 py-1.5 bg-slate-100 dark:text-slate-100 dark:bg-slate-800 text-slate-700 border-slate-200 dark:border-slate-800 focus:border-ring focus:ring-sky-400 text-sm']) }}
+  >
+    {{ $slot }}
+  </select>
+</label>
